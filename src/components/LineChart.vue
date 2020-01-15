b81756d142b3e8ac8b789387962a63b2555415d2
令狐少侠 <czc12580520@gmail.com>
2020-01-15 15:10:52 +0800

    ✅ 添加组件

---
+++ src/components/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <vue-apex-charts
+    ref="lineChart"
+    :type="type"
+    :options="chartData.chartOptions"
+    :series="chartData.series"
+  ></vue-apex-charts>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: 'LineChart',
+  components: { VueApexCharts },
+
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'line',
+    },
+  },
+
+}
+</script>
