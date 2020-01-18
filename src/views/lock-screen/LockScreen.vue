c8130a20e1bc98bfa58fbd45e495c10050dd0790
令狐少侠 <czc12580520@gmail.com>
2020-01-18 23:22:23 +0800

    ✅ 添加组件

---
+++ src/views/lock-screen/LockScreen.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ $store.state.isLocked }}
+    <vs-button @click="unLocked()">解锁</vs-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LockScreen',
+
+  methods: {
+    unLocked() {
+      this.$store.commit('SET_LOCKED', false)
+      this.$router.go(-1)
+    },
+  },
+}
+</script>
