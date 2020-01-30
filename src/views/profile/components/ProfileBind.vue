c26bc956163e9822fa61037df45fa9ead78eb73d
令狐少侠 <czc12580520@gmail.com>
2020-01-30 13:29:58 +0800

    ✅ 添加组件

---
+++ src/views/profile/components/ProfileBind.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-gray px-5 pt-5 flex justify-between radius overflow-hidden">
+    <div class="mr-4 pb-4 flex-col-center">
+      <div class="text-primary text-lg">您好，绑定用户</div>
+      <div class="w-24 mt-3 py-2 text-center text-white text-sm
+     bg-primary-gradient rounded-lg cursor-pointer">
+        绑定用户
+      </div>
+    </div>
+    <img
+      class="w-48"
+      src="@/assets/images/bind_user.svg"
+      alt=""
+    >
+  </div>
+</template>
+// style="background: rgba(var(--vs-primary), 0.9)"
+<script>
+
+export default {
+  name: 'ProfileLog',
+
+  data: () => ({
+    log: {},
+  }),
+
+  methods: {
+  },
+}
+</script>
