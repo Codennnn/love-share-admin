fda7260f55e1f0ac3bfd6a8b0e06eb88e0d59eaf
czc <czc12580520@gmail.com>
2020-02-08 21:56:01 +0800

    ✅ 添加组件

---
+++ src/views/order/components/OrderStep.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-steps
+    align-center
+    :active="stepsData.active"
+  >
+    <el-step
+      v-for="(step, i) in stepsData.steps"
+      :key="i"
+      :title="step.title"
+    >
+      <div slot="description">{{ step.description }}</div>
+    </el-step>
+  </el-steps>
+</template>
+
+<script>
+export default {
+  name: 'OrderStep',
+  props: {
+    stepsData: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
