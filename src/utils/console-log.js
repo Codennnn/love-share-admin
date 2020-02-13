48199af9b81b62046e75ab4c3a43201fce40198d
czc <czc12580520@gmail.com>
2020-02-13 17:59:27 +0800

    ✅ 添加文件

---
+++ src/utils/console-log.js
@@ -0,0 +1,10 @@
+/* eslint max-len: 0 */
+export default () => {
+  if (process.env.NODE_ENV === 'production') {
+    const titleStyle = "padding:6px 0;font-size: 18px;font-family: 'Microsoft YaHei'"
+    const style = "padding:6px 0;font-size: 15px;font-family: 'Microsoft YaHei'"
+    console.log('%c您可以通过以下方式联系我 👇🏼：', titleStyle)
+    console.log('%c📧 Email: czc12580520@gmail.com', style)
+    console.log('%c🌏 Wechat: czc12580520', style)
+  }
+}
