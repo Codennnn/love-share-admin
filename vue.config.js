f70436c9b357e7ab9eab35059673b81a2b867f11
陈梓聪 <chenzc@pvc123.com>
2020-06-04 09:11:15 +0800

    ✔️ 临时提交

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,6 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const IS_PROD = process.env.NODE_ENV === 'production'
 
 const assetsCDN = {
-  publicPath: process.env.VUE_APP_PUBLIC_PATH,
-
   externals: {
     vue: 'Vue',
     'vue-router': 'VueRouter',
@@ -22,6 +20,8 @@ const assetsCDN = {
 }
 
 module.exports = {
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+
   productionSourceMap: false,
 
   // devServer: {
